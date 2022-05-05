50b0fa800612ec13eaa48216c6f3e6c960412c8e
JonaThanPabonP <jpaco95@gmail.com>
2022-05-04 23:35:19 -0500

    Taller4

--- code/week5/baloncesto.py
+++ code/week5/baloncesto.py
@@ -1,8 +1,21 @@
 def menu():
-    print("1. Ingresar jugadores.")
-    print("2. Ingresar partidos.")
-    print("3. Ingresar .")
+    print("1. Ingresar nuevo jugador.")
+    print("2. Ingresar nuevo partido.")
+    print("3. Ingresar datos de jugador en juego.")
+    print("4. Mostrar resultados.")
+    print("5. Mostrar todos los jugadores.")
+    print("0. Salir.")
+
 
 flag = True
 while flag:
+    menu()
+    opc = input("Ingrese la opción necesaria: ")
     
+    if opc == 0:
+        flag = False
+
+    elif opc == 1:
+        nombre = input("Nombre: ")
+        edad = input("Edad: ")
+        
