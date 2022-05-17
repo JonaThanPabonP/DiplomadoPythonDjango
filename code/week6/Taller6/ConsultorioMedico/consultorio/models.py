ccce37b04770409ca42bb2e967b69e91bd6de1bf
yurley <yurs.ksf1@gmail.com>
2022-05-16 20:19:12 -0500

    bad practice remove migration

--- code/week6/Taller6/ConsultorioMedico/consultorio/models.py
+++ code/week6/Taller6/ConsultorioMedico/consultorio/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
@@ -18,6 +19,7 @@ class Doctor(models.Model):
 class Cita_medica(models.Model):
     uniqueId = models.AutoField(primary_key=True)
     ubicacion = models.CharField(max_length=500)
-    fecha = models.DateField(null=True)
+    fecha = models.DateField(default=datetime.now)
     paciente_id = models.ForeignKey('Paciente', on_delete=models.CASCADE)
     doctor_id = models.ForeignKey('Doctor', on_delete=models.CASCADE)
+    diagnostico = models.CharField(max_length=1000, null=True)
