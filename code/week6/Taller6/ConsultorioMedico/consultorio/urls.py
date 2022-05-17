8bda990016eabb5b86bab98af1329894fd40334e
yurley <yurs.ksf1@gmail.com>
2022-05-16 21:19:14 -0500

    create views

--- code/week6/Taller6/ConsultorioMedico/consultorio/urls.py
+++ code/week6/Taller6/ConsultorioMedico/consultorio/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from consultorio import views
 
+app_name = 'consultorio'
 urlpatterns = [
-
-    path('', TemplateView.as_view(template_name = "home.html"), name="home"),
-    
+    path('', TemplateView.as_view(template_name="home.html"), name="home"),
+    path('pacientes', TemplateView.as_view(template_name="pacientes.html"), name="pacientes"),
+    path('medicos', TemplateView.as_view(template_name="medicos.html"), name="medicos"),
+    path('especialidades', TemplateView.as_view(template_name="especialidades.html"), name="especialidades"),
+    path('citas', views.numero_de_citas, name="numero_citas"),
 ]
--- code/week6/Taller6/ConsultorioMedico/consultorio/views.py
+++ code/week6/Taller6/ConsultorioMedico/consultorio/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Cita_medica
 
 # Create your views here.
+
+def numero_de_citas(request):
+    citas = Cita_medica.objects.all()
+
+    context = {
+        "numero_citas": len(citas)
+    }
+    return render(request, 'numero_citas_medicas.html', context)
